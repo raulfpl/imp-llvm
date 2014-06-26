71db4f897790412463f3037d6e59d9151ea1c233
Raul Felipe <raulfpl@gmail.com>
2014-06-26 11:32:47 -0300

    Add codegen for logical or

--- CodeGen/CodeGen.hs
+++ CodeGen/CodeGen.hs
@@ -21,8 +21,8 @@ instance CodeGen VarDef where
 
 instance CodeGen Program where
   type Result Program = Doc
-  gen (Program vs cs) = do 
-                          x <-  liftM (hcat . punctuate nl) (mapM gen vs) 
+  gen (Program vs cs) = do
+                          x <-  liftM (hcat . punctuate nl) (mapM gen vs)
                           x' <- liftM (hcat . punctuate nl) (mapM gen cs)
                           return (hcat $ punctuate nl [header, x, x', end])
 
@@ -32,9 +32,9 @@ instance CodeGen Com where
 
 instance CodeGen AExp where
   type Result AExp = (Doc,Int) -- this Int holds, the variable that has the "result"
-  gen (Const n) = do 
+  gen (Const n) = do
                    v <- fresh
-                   return (text $ concat ["%",show v," = add i32", " 0, ", show n], v)  
+                   return (text $ concat ["%",show v," = add i32", " 0, ", show n], v)
   gen (Var v) = do
                   v' <- fresh
                   return (text $ concat ["%", show v', " = ", "load i32* @",v,",align 4"], v')
@@ -49,22 +49,31 @@ instance CodeGen BExp where
                      v <- fresh
                      let f True  = 1
                          f False = 0
-                     return (text $ concat ["%",show v," = add i32", " 0, ", show (f b)], v)  
+                     return (text $ concat ["%",show v," = add i32", " 0, ", show (f b)], v)
   gen (e :&: e') = do
-                     (c,v) <- gen e
-                     (c',v') <- gen e'
-                     [l, l', r, r1, r2, r3] <- mapM fresh [1..4]
-                     
-                     
+     (c,v) <- gen e
+     (c',v') <- gen e'
+     [l1, r1, l2, r2, l3, r3, r4] <- mapM fresh [1..6]
+     return (text $ concat ["br label %", show l1, "\n",
+                           "; <label>:", show l1, "\n",
+                           "%", show r1, " = icmp ne i32 %", show v, ", 0\n",
+                           "br i1 %", show r1, " label %", show l3, ", label %", show l2, "\n",
+                           "\n",
+                           "; <label>:", show l2, "\n",
+                           "%", show r2, " = icmp ne i32 %", show v', " ,0\n",
+                           "br label %", show l3, "\n",
+                           "\n",
+                           "; <label>:", show l3, "\n",
+                           "%", show r3, " = phi il [ true,%", show l1, " ], [ %", show r2, ", %", show l2, "]\n",
+                           "%", show r4, " = zext i1 %", show r3, " to i32\n"] ,r4)
 
 genBinExp :: AExp -> AExp -> String -> GenM (Result AExp)
 genBinExp e e' op = do
                       (c,v) <- gen e
                       (c',v') <- gen e'
                       r <- fresh
-                      return (text $ concat ["%",show r," = ", op, " nsw i32 %", 
+                      return (text $ concat ["%",show r," = ", op, " nsw i32 %",
                                                 show v, ", %", show v'], r)
-                      
 
 fresh :: GenM Int
 fresh = do
